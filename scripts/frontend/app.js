8d0165fd51c397494af1e2120696a599273d2420
BNSavov19 <BNSavov19@codingburgas.bg>
2021-04-13 00:40:02 +0300

    Clean up navbar hover effect

--- scripts/frontend/app.js
+++ scripts/frontend/app.js
@@ -1,5 +1,17 @@
 const cursor = document.querySelector('.cursor');
+const hoverables = document.querySelectorAll('.hoverable');
+const arr = [...hoverables];
 document.addEventListener('mousemove', (e) => {
     cursor.style.left = e.pageX + 'px';
     cursor.style.top = e.pageY + 'px';
+})
+
+arr.forEach(item => {
+    item.addEventListener("mouseover", ()=>{
+        cursor.classList.add("hovering");
+    })
+
+    item.addEventListener("mouseout", ()=>{
+        cursor.classList.remove("hovering");
+    })
 })
