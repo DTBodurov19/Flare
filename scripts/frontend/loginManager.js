705589e0e3b817b95b2057f3d30997503db53a0b
BNSavov19 <BNSavov19@codingburgas.bg>
2021-04-13 11:24:40 +0300

    Fix login Form

--- scripts/frontend/loginManager.js
+++ scripts/frontend/loginManager.js
@@ -1,6 +1,6 @@
-const form = document.getElementById('log-in-form');
-const Username = document.getElementById('form-username');
-const Password = document.getElementById('form-password');
+const loginForm = document.getElementById('log-in-form');
+const usernameField = document.getElementById('form-username');
+const passwordField = document.getElementById('form-password');
 const messageElement = document.getElementById('message');
 const accManager = new AccountManager;
 
@@ -8,16 +8,20 @@ function validateForm(accmanager, username, password){
     accmanager.login(username.value, password.value);
 }
 
-form.addEventListener('submit', (e) =>{
+loginForm.addEventListener('submit', (e) =>{
     e.preventDefault();
 
     try{
-        validateForm(accManager, Username, Password);
+        validateForm(accManager, usernameField, passwordField);
         messageElement.classList.remove('error');
-        messageElement.innerHTML = `Logged in as a ${accManager.activeUser}`;
+        messageElement.innerHTML = `Logged in as a ${accManager.activeUser.username}`;
     }
     catch (error){
         messageElement.classList.add('error');
-        messageElement.innerHTML = error;
+        messageElement.innerHTML = error.message;
+    }
+    finally {
+        usernameField.value = '';
+        passwordField.value = '';
     }
 });
