5688bca7bfcdb29ec596b646feb0720039aadb24
Kalin Chervenkov <KSChervenkov19@codingburgas.bg>
2021-04-20 01:46:04 +0300

    Add checking input script
      - style the type buttons
      - change the fillData logic
      - change the transparent logo name

--- scripts/frontend/fireReportManager.js
+++ scripts/frontend/fireReportManager.js
@@ -1,17 +1,45 @@
-function validateReport(){
-    let obj = {
-        fName: document.querySelector('#report-fname').value,
-        sName: document.querySelector('#report-sname').value,
-        lName: document.querySelector('#report-lname').value,
+let reportFormData;
+
+function fillData(){
+    reportFormData = {
+        name: {
+            fName: document.querySelector('#report-fname').value,
+            sName: document.querySelector('#report-sname').value,
+            lName: document.querySelector('#report-lname').value
+        },
         phoneNumber: document.querySelector('#phone-number').value,
         location: document.querySelector('#location').value,
-        info: document.querySelector('#info').value
+        info: document.querySelector('#info').value,
+        type: document.querySelector('.fire-button').value
     };
-
-    return obj;
 }
+
+function checkEmpty() {
+    if (reportFormData.name.fName == "") {
+        alert("Enter your first name");
+        
+    }
+    else if (reportFormData.name.sName == "") {
+        alert("Enter your surname");
+    }
+    else if (reportFormData.name.lName == "") {
+        alert("Enter your last name");
+    }
+    else if (reportFormData.phoneNumber == "") {
+        alert("Enter your phone number");
+    }
+    else if (reportFormData.location == "") {
+        alert("Enter your location");
+    }
+    else {
+        alert("You are all set");
+        console.log (reportFormData);
+    }
+}
+
 
 reportForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(validateReport());
+    fillData();
+    checkEmpty();
 });
