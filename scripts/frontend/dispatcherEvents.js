225ad8fa2b6f2a2e84685caa21413876606a8560
BNSavov19 <BNSavov19@codingburgas.bg>
2021-04-22 20:29:03 +0300

    Add event validate button, closes #12

--- scripts/frontend/dispatcherEvents.js
+++ scripts/frontend/dispatcherEvents.js
@@ -1,14 +1,45 @@
 const FireEventsManager1 = new FireEventsManager(localStorage);
 const eventsContainer = document.querySelector('.events');
 
+
+
+function validateEvent(button) {
+  let id = button.className.split(' ')[1];
+  FireEventsManager1.validateFireEvent(+id);  
+  location.reload();
+}
+
 function getEvents(events) {
   for (const event of events) {
+    
+    let eventContainer = document.createElement('div');
+
     let eventList = document.createElement('li');
     let textNode = document.createTextNode(`${event.description} ${event.fireSize} ${event.lat} ${event.long} ${event.reporterName} ${event.startDate} ${event.state} ${event.telephoneNumber}`);
+    
     eventList.appendChild(textNode);
-    eventsContainer.appendChild(eventList);
+    eventContainer.appendChild(eventList);
     eventList.classList.add('event');
     eventList.classList.add(event.ID.toString());
+
+    let verifyButton = document.createElement('button');
+    let verifyButtonNode = document.createTextNode('Validate');
+
+    verifyButton.appendChild(verifyButtonNode);
+    eventContainer.appendChild(verifyButton);
+    verifyButton.classList.add('validate-button');
+    verifyButton.classList.add(event.ID.toString());
+    
+
+    eventsContainer.appendChild(eventContainer);
+    eventContainer.classList.add('eventContainer');
+
+    let validateButtons = [...document.querySelectorAll('.validate-button')];
+    for(button of validateButtons) {
+      button.addEventListener('click', () => {
+        validateEvent(button);
+      });
+    }
   }
 }
 
