0fb397cabf12dd5b6cd7ecdefcb7fd22fd651c89
Kalin Chervenkov <KSChervenkov19@codingburgas.bg>
2021-04-25 01:34:21 +0300

    Add animation on driver map
      - fix the change marker position on grab issue

--- scripts/frontend/Driver/driverMap.js
+++ scripts/frontend/Driver/driverMap.js
@@ -4,10 +4,21 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+let leafletIcon = L.icon ({
+  iconUrl: 'https://svgshare.com/i/AaW.svg',
+  iconSize: [20,20],
+  iconAnchor: [10,10],
+});
+
 let control = L.Routing.control({
+  draggableWaypoints: false,
+  addWaypoints: false,
   waypoints: [
       L.latLng(42.49473180801061, 27.4663021042943),
-      L.latLng(42.52172156626854, 27.467746138572693)
+      L.latLng([42.52172156626854, 27.467746138572693], {icon:leafletIcon})
   ],
+  lineOptions: {
+    styles: [{className: 'animate'}]
+},
   routeWhileDragging: true
 }).addTo(map);
