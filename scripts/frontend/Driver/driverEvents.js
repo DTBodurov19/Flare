a6fbbf47b25b1520b7e9971370a4764dece2379e
BNSavov19 <BNSavov19@codingburgas.bg>
2021-04-24 12:56:39 +0300

    Add driver site
      - Rearrange style and script files

---
+++ scripts/frontend/Driver/driverEvents.js
@@ -0,0 +1,52 @@
+const fireEventsManager = new FireEventsManager(localStorage);
+const eventsContainer = document.querySelector(".events");
+
+function startEvent(button) {
+  let id = button.className.split(" ")[1];
+  fireEventsManager.validateFireEvent(+id);
+  console.log(`Validated ${id}`);
+}
+
+function getEvents(events) {
+  eventsContainer.innerHTML = "";
+
+  for (const event of events) {
+    let eventContainer = document.createElement("div");
+
+    let eventList = document.createElement("li");
+    let textNode = document.createTextNode(
+      `${event.description} ${event.fireSize} ${event.lat} ${event.long} ${event.reporterName} ${event.startDate} ${event.state} ${event.telephoneNumber}`
+    );
+
+    eventList.appendChild(textNode);
+    eventContainer.appendChild(eventList);
+    eventList.classList.add("event");
+    eventList.classList.add(event.ID.toString());
+
+    let startButton = document.createElement("button");
+    let startButtonNode = document.createTextNode("Start Event");
+
+    startButton.appendChild(startButtonNode);
+    eventContainer.appendChild(startButton);
+    startButton.classList.add("start-event-button");
+    startButton.classList.add(event.ID.toString());
+
+    eventsContainer.appendChild(eventContainer);
+    eventContainer.classList.add("eventContainer");
+  }
+
+  let startButtons = [...document.querySelectorAll(".start-event-button")];
+  for (const button of startButtons) {
+    button.addEventListener("click", () => {
+      validateEvent(button);
+      getEvents(
+        fireEventsManager.getFireEventsByState(
+          FireEventsManager.fireStates.pending
+        )
+      );
+      removeCurrentMarker(button);
+    });
+  }
+}
+
+window.onload = getEvents(fireEventsManager.getFireEventsByState(FireEventsManager.fireStates.pending));
